dd6fed1224dd6ca86a0f6f7933b2bdd0fe8e5e77
蒼時弦也 <elct9620@frost.tw>
2013-10-31 10:37:08 +0800

    Update# make course support query by name, course code, class code

--- Resources/Course.php
+++ Resources/Course.php
@@ -1,20 +1,51 @@
 <?php
 
+// All courses
 $app->get('/courses', function() use($app) {
-  $page = $app->request->getQuery('page', null, 1);
+  $page = $app->request->getQuery('page', 'int', 1);
   $startRecord = ($page - 1) * PER_PAGE;
-  $phql = "SELECT * FROM Courses LIMIT {$startRecord}, " . PER_PAGE;
-  $courses = $app->modelsManager->executeQuery($phql);
+
+  // Query
+  $queryString = array();
+  $queryData = array();
+  $name = $app->request->getQuery('name');
+  $courseCode = $app->request->getQuery('course_code');
+  $classCode = $app->request->getQuery('class_code');
+
+  if($name) {
+    array_push($queryString, 'course_name LIKE :name:');
+    $queryData['name'] = '%' . $name . '%';
+  }
+
+  if($courseCode) {
+    array_push($queryString, 'course_code LIKE :courseCode:');
+    $queryData['courseCode'] = '%'. $courseCode . '%';
+  }
+
+  if($classCode) {
+    array_push($queryString, 'class_code LIKE :classCode:');
+    $queryData['classCode'] = '%' . $classCode . '%';
+  }
+
+  if(count($queryString) > 0) {
+    $queryCache = implode(" AND ", $queryString);
+    $queryString = " WHERE {$queryCache}";
+    unset($queryCache);
+  } else {
+    $queryString = "";
+  }
+
+  $phql = "SELECT * FROM Courses{$queryString} LIMIT {$startRecord}, " . PER_PAGE;
+  $courses = $app->modelsManager->executeQuery($phql, $queryData);
 
   return $app->response->setJsonContent($courses->toArray());
 });
 
-$app->get('/courses/{name}', function($name) use ($app) {
-  $page = $app->request->getQuery('page', null, 1);
-  $startRecord = ($page - 1) * PER_PAGE;
-  $phql = "SELECT * FROM Courses WHERE course_name LIKE :name: LIMIT {$startRecord}, " . PER_PAGE;
-  $courses = $app->modelsManager->executeQuery($phql, array('name' => '%' . $name . '%'));
+// Get course by id
+$app->get('/course/{id:[0-9]+}', function($id) use ($app) {
+  $phql = "SELECT * FROM Courses WHERE id = :id:";
+  $course = $app->modelsManager->executeQuery($phql, array('id' => $id));
 
-  return $app->response->setJsonContent($courses->toArray());
+  return $app->response->setJsonContent($course->toArray());
 });
 
