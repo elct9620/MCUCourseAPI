2b9390ba7c77c377f42f55bc4771115c1b8775fe
蒼時弦也 <elct9620@frost.tw>
2013-11-14 22:48:26 +0800

    Refactor# refactor department query via QueryHelper

--- Resources/Department.php
+++ Resources/Department.php
@@ -1,37 +1,21 @@
 <?php
 
 $app->get('/departments', function() use ($app) {
-  $page = $app->request->getQuery('page', 'int', 1);
-  $startRecord = ($page - 1) * PER_PAGE;
 
-  // Query
-  $queryString = array();
-  $queryData = array();
-  $name = $app->request->getQuery('name');
+  $queryHelper = $app->queryHelper;
+  $queryHelper->setModel(Departments::query());
+  $queryHelper->addFilter('name');
+  $queryHelper->usePage(PER_PAGE);
 
-  if($name) {
-    array_push($queryString, 'name LIKE :name:');
-    $queryData['name'] = '%' . $name . '%';
-  }
-
-  if(count($queryString) > 0) {
-    $queryCache = implode(" AND ", $queryString);
-    $queryString = " WHERE {$queryCache}";
-    unset($queryCache);
-  } else {
-    $queryString = "";
-  }
-
-  $phql = "SELECT * FROM Departments{$queryString} LIMIT {$startRecord}, " . PER_PAGE;
-  $departments = $app->modelsManager->executeQuery($phql, $queryData);
-
-  return $app->response->setJsonContent($departments->toArray());
+  return $app->response->setJsonContent($queryHelper->result());
 });
 
 $app->get('/department/{code:[0-9]+}', function($code) use ($app) {
-  $phql = "SELECT * FROM Departments WHERE code = :code:";
-  $department = $app->modelsManager->executeQuery($phql, array('code' => $code));
 
-  return $app->response->setJsonContent($department->toArray());
+  $queryHelper = $app->queryHelper;
+  $queryHelper->setModel(Departments::query());
+  $queryHelper->addFilter('code', QueryHelper::FILTER_SIMPLE, $code);
+
+  return $app->response->setJsonContent($queryHelper->result());
 
 });
