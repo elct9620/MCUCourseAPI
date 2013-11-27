04c84a18ab3bef765ec819232aeeaa8f2369f211
蒼時弦也 <elct9620@frost.tw>
2013-11-27 10:44:24 +0800

    Update# change update date to 2013-11-27

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 
 // Version and Update
 define('VERSION', '0.1.0');
-define('LAST_UPDATE', '2013-11-15');
+define('LAST_UPDATE', '2013-11-27');
 define('RESOURCE_PATH', dirname(__FILE__) . '/Resources');
 define('PER_PAGE', 25);
