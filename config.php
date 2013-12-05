18dfbbb133c07d30432ac567038a0fb1f96bc6d2
蒼時弦也 <elct9620@frost.tw>
2013-12-05 22:37:19 +0800

    Update# update database

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 
 // Version and Update
 define('VERSION', '0.1.1');
-define('LAST_UPDATE', '2013-11-27');
+define('LAST_UPDATE', '2013-12-05');
 define('RESOURCE_PATH', dirname(__FILE__) . '/Resources');
 define('PER_PAGE', 25);
