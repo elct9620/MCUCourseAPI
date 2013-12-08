c122541d11d29ea099ea1189d82d453584fd276f
蒼時弦也 <elct9620@frost.tw>
2013-12-08 18:08:22 +0800

    Update# add simple times to course for query course

--- Models/Courses.php
+++ Models/Courses.php
@@ -6,6 +6,7 @@ use Phalcon\Mvc\Model;
 
 class Courses extends Model
 {
+    public $simple_times;
     protected $created_at;
     protected $updated_at;
 
@@ -14,5 +15,35 @@ class Courses extends Model
         $this->hasMany('id', 'MCUCourseAPI\Models\Teachers', 'course_id', array(
           'alias' => 'Teachers'
         ));
+    }
+
+    public function afterFetch()
+    {
+        $this->simple_times = $this->getSimpleTimes();
+    }
+
+    protected function getSimpleTimes()
+    {
+        $teachers = $this->getTeachers();
+
+        $simpleTimes = array();
+        $times = null;
+        $timesItem = null;
+        foreach ($teachers as $teacher) {
+            $times = $teacher->getCourseTimes();
+
+            $timesItem = array();
+            foreach ($times as $time) {
+                array_push($timesItem, $time->time);
+            }
+
+            array_push($simpleTimes, array(
+              'teacher' => $teacher->teacher,
+              'camps' => $teacher->camps,
+              'day' => $teacher->course_day,
+              'times' => $timesItem
+            ));
+        }
+        return $simpleTimes;
     }
 }
