e6b4a6b8c8dfba56b0fc3f48c5e9977726b39210
蒼時弦也 <elct9620@frost.tw>
2013-12-08 20:14:38 +0800

    Update# update course database

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 
 // Version and Update
 define('VERSION', '0.1.1');
-define('LAST_UPDATE', '2013-12-05');
+define('LAST_UPDATE', '2013-12-08');
 define('RESOURCE_PATH', dirname(__FILE__) . '/Resources');
 define('PER_PAGE', 25);
